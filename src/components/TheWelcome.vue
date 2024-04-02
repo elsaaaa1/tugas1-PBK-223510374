01bb8b65cd2b3ac55ce4894353a7e3ad873a32bb
elsa <elsaputri2038@gmail.com>
2024-04-03 00:01:06 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>pendidikan</template>
+
+    TK Dharma Wanita, SDN 005 Bagan Laguh, SMP N 2 BUNUT, SMA N 1 BUNUT 
+   
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Tempat tanggal lahir</template>
+
+    
+
+    BAGAN LAGUH, 28 SEPTEMBER 2004 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Alamat</template>
+
+    Pangkalan Kuras Pelalawan
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Prestasi</template>
+
+  pernah menjadi ketua OSIS di smp dan wakil ketua OSIS di SMA
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Pengalaman </template>
+
+menjadi panitia KPPS dan panitia di berbagai acara waktu sekolah
+  </WelcomeItem>
+</template>
